380178051b47ef37b5030c612582b5b5c963be6c
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-06 16:10:36 +0530

    terraform

--- main.tf
+++ main.tf
@@ -27,7 +27,8 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index] # we need to add this from argument reference
+  map_public_ip_on_launch = true # this is only for public subnet
   tags = merge(
     var.common_tags,
     var.public_subnet_tags,
