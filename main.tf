328dc7e2367320fa97c93a2e7a63daba9128cdf5
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-06 16:07:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -36,3 +36,31 @@ resource "aws_subnet" "public" {
     }
   )
 }
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  tags = merge(
+    var.common_tags,
+    var.private_subnet_tags,
+    {
+        Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_tags,
+    {
+        Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+    }
+  )
+}
--- variables.tf
+++ variables.tf
@@ -38,3 +38,27 @@ variable "public_subnet_cidrs" {
 variable "public_subnet_tags" {
   default = {}
 }
+
+variable "private_subnet_cidrs" {
+  type = list 
+  validation { 
+    condition = length(var.private_subnet_cidrs) == 2
+    error_message = "please provide 2 valid private subnet CIDR"
+  } 
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type = list 
+  validation { 
+    condition = length(var.database_subnet_cidrs) == 2
+    error_message = "please provide 2 valid database subnet CIDR"
+  } 
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
