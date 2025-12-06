a97f4c1fc16ee1411b160860286c63bc9f237adb
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-06 12:13:51 +0530

    terraform

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ resource "aws_internet_gateway" "main" {
     }
   )
 }
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+    
+  tags = {
+    Name = "Main"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -26,3 +26,11 @@ variable "vpc_tags" {
 variable "igw_tags" {
   default = {}
 }
+
+variable "public_subnet_cidrs" {
+  type = list 
+  validation { # search terraform variable size validation, we are giving 2 as per our expense project requirement hence we are asking user to supply only 2 public subnet CIDR
+    condition = length(var.public_subnet_cidrs) == 2
+    error_message = "please provide 2 valid public subnet CIDR"
+  } 
+}
