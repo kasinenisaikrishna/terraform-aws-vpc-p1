2fc3ecd73a8f5a44c7a1c7b53eea48e2eb15623e
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-07 22:03:06 +0530

    terraform

--- data.tf
+++ data.tf
@@ -6,6 +6,10 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_route_table" "main" {
+data "aws_route_table" "main" { # aws main route table data source
   vpc_id = data.aws_vpc.default.id
+  filter {
+    Name = "association.main"
+    value = true
+  }
 }
